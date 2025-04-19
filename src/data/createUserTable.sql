48a4c6fb7488b7029dec62318353211bde6910af
Hari Prasad <hp1234v@gmail.com>
2025-04-19 17:48:27 +0530

    update create statement

--- src/data/createUserTable.sql
+++ src/data/createUserTable.sql
@@ -5,8 +5,8 @@
 CREATE TABLE IF NOT EXISTS todo.users
 (
     id integer NOT NULL DEFAULT nextval('todo.users_id_seq'::regclass),
-    name character varying(100)[] COLLATE pg_catalog."default" NOT NULL,
-    email character varying(100)[] COLLATE pg_catalog."default" NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default" NOT NULL,
     created_on timestamp without time zone NOT NULL DEFAULT now(),
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT constraint_name UNIQUE (email)
