a841f21f61e551a60aee7d43184392782e0d1f67
Hari Prasad <hp1234v@gmail.com>
2025-04-19 18:40:44 +0530

    added docker yml file

--- src/data/createUserTable.sql
+++ src/data/createUserTable.sql
@@ -2,6 +2,13 @@
 
 -- DROP TABLE IF EXISTS todo.users;
 
+CREATE SEQUENCE IF NOT EXISTS todo.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 CREATE TABLE IF NOT EXISTS todo.users
 (
     id integer NOT NULL DEFAULT nextval('todo.users_id_seq'::regclass),
